3e2f0af3fc390112afe5dc29f74876f8a02df8e4
Liu Xinquan <xqliu@talend.com>
2020-09-09 15:23:54 +0800

    chore(TDQ-18744) comment out powermock/mockito (#777)

--- test/plugins/org.talend.cwm.management.test/src/org/talend/dq/helper/ProxyRepositoryManagerTest.java
+++ test/plugins/org.talend.cwm.management.test/src/org/talend/dq/helper/ProxyRepositoryManagerTest.java
@@ -12,71 +12,56 @@
 // ============================================================================
 package org.talend.dq.helper;
 
-import static org.mockito.Mockito.*;
-import static org.powermock.api.support.membermodification.MemberMatcher.*;
-import static org.powermock.api.support.membermodification.MemberModifier.*;
-import junit.framework.Assert;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.rule.PowerMockRule;
-import org.talend.commons.runtime.model.repository.ERepositoryStatus;
-import org.talend.core.model.properties.Item;
-import org.talend.core.repository.model.ProxyRepositoryFactory;
-
 /**
  * DOC yyin class global comment. Detailled comment
  */
-@PrepareForTest({ ProxyRepositoryFactory.class })
+// @PrepareForTest({ ProxyRepositoryFactory.class })
 public class ProxyRepositoryManagerTest {
 
-    @Rule
-    public PowerMockRule powerMockRule = new PowerMockRule();
-
-    private ProxyRepositoryFactory proxFactory;
-
-    /**
-     * DOC yyin Comment method "setUp".
-     * 
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        proxFactory = mock(ProxyRepositoryFactory.class);
-        stub(method(ProxyRepositoryFactory.class, "getInstance")).toReturn(proxFactory);//$NON-NLS-1$
-
-    }
-
-    /**
-     * DOC yyin Comment method "tearDown".
-     * 
-     * @throws java.lang.Exception
-     */
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    /**
-     * Test method for
-     * {@link org.talend.dq.helper.ProxyRepositoryManager#isLocked(org.talend.core.model.properties.Item)}.
-     */
-    @Test
-    public void testIsLocked() {
-        Item item = mock(Item.class);
-        when(proxFactory.getStatus(item)).thenReturn(ERepositoryStatus.LOCK_BY_USER);
-        Assert.assertTrue(ProxyRepositoryManager.getInstance().isLocked(item));
-
-        when(proxFactory.getStatus(item)).thenReturn(ERepositoryStatus.LOCK_BY_OTHER);
-        Assert.assertTrue(ProxyRepositoryManager.getInstance().isLocked(item));
-
-        when(proxFactory.getStatus(item)).thenReturn(ERepositoryStatus.DEFAULT);
-        Assert.assertFalse(ProxyRepositoryManager.getInstance().isLocked(item));
-
-        when(proxFactory.getStatus(item)).thenReturn(ERepositoryStatus.READ_ONLY);
-        Assert.assertFalse(ProxyRepositoryManager.getInstance().isLocked(item));
-    }
+    // @Rule
+    // public PowerMockRule powerMockRule = new PowerMockRule();
+    //
+    // private ProxyRepositoryFactory proxFactory;
+    //
+    // /**
+    // * DOC yyin Comment method "setUp".
+    // *
+    // * @throws java.lang.Exception
+    // */
+    // @Before
+    // public void setUp() throws Exception {
+    // proxFactory = mock(ProxyRepositoryFactory.class);
+    // stub(method(ProxyRepositoryFactory.class, "getInstance")).toReturn(proxFactory);//$NON-NLS-1$
+    //
+    // }
+    //
+    // /**
+    // * DOC yyin Comment method "tearDown".
+    // *
+    // * @throws java.lang.Exception
+    // */
+    // @After
+    // public void tearDown() throws Exception {
+    // }
+    //
+    // /**
+    // * Test method for
+    // * {@link org.talend.dq.helper.ProxyRepositoryManager#isLocked(org.talend.core.model.properties.Item)}.
+    // */
+    // @Test
+    // public void testIsLocked() {
+    // Item item = mock(Item.class);
+    // when(proxFactory.getStatus(item)).thenReturn(ERepositoryStatus.LOCK_BY_USER);
+    // Assert.assertTrue(ProxyRepositoryManager.getInstance().isLocked(item));
+    //
+    // when(proxFactory.getStatus(item)).thenReturn(ERepositoryStatus.LOCK_BY_OTHER);
+    // Assert.assertTrue(ProxyRepositoryManager.getInstance().isLocked(item));
+    //
+    // when(proxFactory.getStatus(item)).thenReturn(ERepositoryStatus.DEFAULT);
+    // Assert.assertFalse(ProxyRepositoryManager.getInstance().isLocked(item));
+    //
+    // when(proxFactory.getStatus(item)).thenReturn(ERepositoryStatus.READ_ONLY);
+    // Assert.assertFalse(ProxyRepositoryManager.getInstance().isLocked(item));
+    // }
 
 }
