2a3e3e744688d2205fe70ad5adf5aa787f493b12
bhe-talendbj <bhe@talend.com>
2021-07-02 09:45:34 +0800

    feat(TUP-31487): add new method to ICoreService

--- main/plugins/org.talend.dataprofiler.rcp/src/org/talend/dataprofiler/rcp/TopService.java
+++ main/plugins/org.talend.dataprofiler.rcp/src/org/talend/dataprofiler/rcp/TopService.java
@@ -501,4 +501,10 @@ public class TopService implements ICoreService {
         ProxyRepositoryFactory.getInstance().executeRepositoryWorkUnit(workUnit);
     }
 
+    @Override
+    public void installComponents() {
+        // TODO Auto-generated method stub
+        
+    }
+
 }
